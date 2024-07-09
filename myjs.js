79f7f190e3249c5266ba138754720c7d402d1409
SUDAAZUSA <167832722+SUDAAZUSA@users.noreply.github.com>
2024-07-09 10:30:14 +0900

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -1,12 +1,11 @@
-alert("Hello,Suda!")
+alert('Hello,Jurina!');
 alert(1+1);
 alert(Math.sqrt(1+1));
 alert(Math.sqrt(2+3));
-alert(1/0);
-alert(0/0);
-
+alert(Math.sqrt(1/0));
+alert(Math.sqrt(0/0));
 alert(Math.sqrt(2+3));
 alert(Math.sqrt('2'+'3'));
-alert(Math.sqrt(Number('2')+Number('3'));
-alert(Math.sqrt('2'+3));
+alert(Math.sqrt(Number('2')+Number('3')));
+alert(Math.sqrt('2+3'));
 alert(Math.sqrt(Number('2')+3));
